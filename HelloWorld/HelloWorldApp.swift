a31b94532dc78c1059a48203d1e396be2d080228
Aaron <aaron911911@icloud.com>
2024-03-24 11:30:23 +0800

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Aaron Yun on 24/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
